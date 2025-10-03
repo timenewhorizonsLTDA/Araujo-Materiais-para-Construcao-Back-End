7658d01f07b29dd5a6fdc40c52be0b945e99f505
vitorGarcia016 <vitorgarciaoliveira2005@gmail.com>
2025-10-03 18:44:49 -0300

    Commit Inicial

---
+++ src/main/java/com/materiais/araujo/araujo_materiais_api/AraujoMateriaisApiApplication.java
@@ -0,0 +1,13 @@
+package com.materiais.araujo.araujo_materiais_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AraujoMateriaisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AraujoMateriaisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/materiais/araujo/araujo_materiais_api/AraujoMateriaisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.materiais.araujo.araujo_materiais_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AraujoMateriaisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
