64cf32e1e7b665563d0b49094c129785fe5f5d38
vitorGarcia016 <vitorgarciaoliveira2005@gmail.com>
2025-10-04 10:43:49 -0300

    implementa ConfiguracaoSeguranca
    Classe de configuracao do Spring Security

---
+++ src/main/java/com/materiais/araujo/araujo_materiais_api/infra/security/ConfiguracaoSeguranca.java
@@ -0,0 +1,24 @@
+package com.materiais.araujo.araujo_materiais_api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracaoSeguranca {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .csrf(c -> c.disable())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+    }
+
+}
