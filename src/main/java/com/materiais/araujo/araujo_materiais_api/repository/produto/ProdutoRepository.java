76a034f54d5a9360b974633bb8a2878d4dc0f810
iago-silves <iago.rodrig.silvestre@gmail.com>
2025-10-08 10:26:32 -0300

    implementa correção de repository

--- src/main/java/com/materiais/araujo/araujo_materiais_api/repository/produto/ProdutoRepository.java
+++ src/main/java/com/materiais/araujo/araujo_materiais_api/repository/produto/ProdutoRepository.java
@@ -2,6 +2,8 @@ package com.materiais.araujo.araujo_materiais_api.repository.produto;
 
 import com.materiais.araujo.araujo_materiais_api.model.produto.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 }
