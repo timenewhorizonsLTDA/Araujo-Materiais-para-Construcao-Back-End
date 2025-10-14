3f2aeb937a2b12ee5f395aa891661265ec3ae486
iago-silves <iago.rodrig.silvestre@gmail.com>
2025-10-14 09:50:10 -0300

    implementa correção de enum

--- src/main/java/com/materiais/araujo/araujo_materiais_api/model/produto/TipoProduto.java
+++ src/main/java/com/materiais/araujo/araujo_materiais_api/model/produto/TipoProduto.java
@@ -1,17 +1,9 @@
 package com.materiais.araujo.araujo_materiais_api.model.produto;
 
 public enum TipoProduto {
-    CIMENTO,
-    AREIA,
-    BRITA,
-    TIJOLO,
-    BLOCO_CONCRETO,
-    FERRO,
-    TUBO_PVC,
+    MATERIA_PRIMA,
+    ESTRUTURAL,
     MADEIRA,
-    TELHA,
-    ARGAMASSA,
-    TINTA,
     REVESTIMENTO,
     FERRAMENTA,
     ELETRICO,
