76262964e6b15d022e9f902f3a31d6207a6bcfff
iago-silves <iago.rodrig.silvestre@gmail.com>
2025-11-08 02:37:45 -0300

    implementa consultas de dividas e agendamentos

--- src/main/java/com/materiais/araujo/araujo_materiais_api/controller/cliente/ClienteController.java
+++ src/main/java/com/materiais/araujo/araujo_materiais_api/controller/cliente/ClienteController.java
@@ -1,6 +1,10 @@
 package com.materiais.araujo.araujo_materiais_api.controller.cliente;
 
+import com.materiais.araujo.araujo_materiais_api.DTO.agendamento.SolicitacaoProdutoResponseDTO;
+import com.materiais.araujo.araujo_materiais_api.DTO.divida.DividaResponseDTO;
 import com.materiais.araujo.araujo_materiais_api.DTO.produto.ProdutoDTO;
+import com.materiais.araujo.araujo_materiais_api.model.divida.StatusDivida;
+import com.materiais.araujo.araujo_materiais_api.model.solicitacaoproduto.StatusSolicitacao;
 import com.materiais.araujo.araujo_materiais_api.service.cliente.ClienteService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,5 +26,21 @@ public class ClienteController {
         return clienteService.consultarProdutosDoOrcamento(id);
     }
 
-}
+    @GetMapping("/agendamentos")
+    public ResponseEntity<List<SolicitacaoProdutoResponseDTO>> verificarAgendamentosCliente(
+            @RequestParam(required = false) StatusSolicitacao status,
+            @RequestParam(required = false) Integer clienteId) {
 
+        return clienteService.verificarAgendamentos(status, clienteId);
+    }
+
+    @GetMapping("/dividas")
+    public ResponseEntity<List<DividaResponseDTO>> verificarDividasCliente(
+            @RequestParam(required = false) StatusDivida status,
+            @RequestParam(required = false) Integer clienteId) {
+
+        return clienteService.verificarDividasCliente(status, clienteId);
+    }
+
+}
+    
--- src/main/java/com/materiais/araujo/araujo_materiais_api/service/cliente/ClienteService.java
+++ src/main/java/com/materiais/araujo/araujo_materiais_api/service/cliente/ClienteService.java
@@ -1,16 +1,22 @@
 package com.materiais.araujo.araujo_materiais_api.service.cliente;
 
+import com.materiais.araujo.araujo_materiais_api.DTO.agendamento.SolicitacaoProdutoResponseDTO;
+import com.materiais.araujo.araujo_materiais_api.DTO.divida.DividaResponseDTO;
 import com.materiais.araujo.araujo_materiais_api.DTO.produto.ProdutoDTO;
 import com.materiais.araujo.araujo_materiais_api.infra.exceptions.personalizadas.cliente.ClienteNaoEncontradoException;
+import com.materiais.araujo.araujo_materiais_api.model.divida.StatusDivida;
 import com.materiais.araujo.araujo_materiais_api.model.orcamento.Orcamento;
+import com.materiais.araujo.araujo_materiais_api.model.solicitacaoproduto.StatusSolicitacao;
 import com.materiais.araujo.araujo_materiais_api.model.usuario.RoleUsuario;
 import com.materiais.araujo.araujo_materiais_api.repository.orcamento.OrcamentoRepository;
 import com.materiais.araujo.araujo_materiais_api.repository.usuario.UsuarioRepository;
+import com.materiais.araujo.araujo_materiais_api.service.funcionario.FuncionarioService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
@@ -20,11 +26,14 @@ public class ClienteService {
 
     private final UsuarioRepository usuarioRepository;
     private final OrcamentoRepository orcamentoRepository;
+    private FuncionarioService funcionarioService;
 
     public ClienteService(UsuarioRepository usuarioRepository,
-                          OrcamentoRepository orcamentoRepository) {
+                          OrcamentoRepository orcamentoRepository,
+                          FuncionarioService funcionarioService) {
         this.usuarioRepository = usuarioRepository;
         this.orcamentoRepository = orcamentoRepository;
+        this.funcionarioService = funcionarioService;
     }
 
     @Transactional
@@ -46,4 +55,21 @@ public class ClienteService {
         return ResponseEntity.ok(produtos);
     }
 
+    public ResponseEntity<List<SolicitacaoProdutoResponseDTO>> verificarAgendamentos(
+            StatusSolicitacao status, Integer clienteId) {
+
+        return funcionarioService.verificarAgendamentos(
+                Optional.ofNullable(status),
+                Optional.ofNullable(clienteId)
+        );
+    }
+
+    public ResponseEntity<List<DividaResponseDTO>> verificarDividasCliente(
+            StatusDivida status, Integer clienteId) {
+
+        return funcionarioService.verificarDividasCliente(
+                Optional.ofNullable(status),
+                Optional.ofNullable(clienteId)
+        );
+    }
 }
